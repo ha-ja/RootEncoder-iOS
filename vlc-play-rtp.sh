8a9f07413b0d1744443fe288eb948888b9cf9bbd
Halder, Jannis <jannis.halder@exxeta.com>
2025-09-23 11:54:06 +0200

    vlc parameters for lower latency

--- vlc-play-rtp.sh
+++ vlc-play-rtp.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/Applications/VLC.app/Contents/MacOS/VLC stream.sdp
+/Applications/VLC.app/Contents/MacOS/VLC --network-caching=0 --clock-jitter=0 --clock-synchro=0 --no-audio --codec=h264 stream.sdp
