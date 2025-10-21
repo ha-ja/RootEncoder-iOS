82a12ce717035c6ac1221bb7f3fea5e5c2d84585
Paul Wyszynski <paul.wyszynski@gmail.com>
2025-10-21 10:32:13 +0200

    Remove unused isVideoFrame function from RtpFrame struct

--- RootEncoder/Sources/RootEncoder/rtsp/rtsp/RtpFrame.swift
+++ RootEncoder/Sources/RootEncoder/rtsp/rtsp/RtpFrame.swift
@@ -5,8 +5,4 @@ public struct RtpFrame {
     var length: Int
     var timeStamp: UInt64
     var channelIdentifier: Int
-    
-    public func isVideoFrame() -> Bool {
-        channelIdentifier == RtpConstants.trackVideo
-    }
 }
